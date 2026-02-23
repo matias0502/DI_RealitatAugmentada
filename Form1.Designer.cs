1ac0cb33a1c1dd87b5cc2168fdc57f9261600f85
matias0502 <maro014@vidalibarraquer.net>
2026-02-23 20:48:56 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace realitatAugmentada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnEscanejar = new System.Windows.Forms.Button();
+            this.pbVideo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEscanejar
+            // 
+            this.btnEscanejar.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnEscanejar.Location = new System.Drawing.Point(268, 390);
+            this.btnEscanejar.Name = "btnEscanejar";
+            this.btnEscanejar.Size = new System.Drawing.Size(136, 30);
+            this.btnEscanejar.TabIndex = 0;
+            this.btnEscanejar.Text = "Escaneja";
+            this.btnEscanejar.UseVisualStyleBackColor = false;
+            // 
+            // pbVideo
+            // 
+            this.pbVideo.Location = new System.Drawing.Point(213, 133);
+            this.pbVideo.Name = "pbVideo";
+            this.pbVideo.Size = new System.Drawing.Size(261, 211);
+            this.pbVideo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbVideo.TabIndex = 1;
+            this.pbVideo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbVideo);
+            this.Controls.Add(this.btnEscanejar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEscanejar;
+        private System.Windows.Forms.PictureBox pbVideo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Drawing;
+using System.Windows.Forms;
+using System;
+using Emgu.CV.Util;
+
+
+namespace realitatAugmentada
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private bool _detectarAR = false;
+        public Form1()
+        {
+            InitializeComponent();
+            _capture = new VideoCapture(0);
+            timer1.Interval = 30;
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (Mat frame = _capture.QueryFrame())
+            {
+                if (frame != null)
+                {
+                    if (_detectarAR)
+                    {
+                        AplicarlogicaAR(frame);
+                    }
+                    if (pbVideo.Image != null) pbVideo.Image.Dispose();
+                    pbVideo.Image = frame.ToBitmap();
+
+                }
+            }
+
+        }
+        private void AplicarlogicaAR(Mat frame)
+        {
+            using (QRCodeDetector detector = new QRCodeDetector())
+            {
+                using (Mat punts = new Mat())
+                {
+                    bool trobat = detector.Detect(frame, punts);
+
+                    if (trobat && !punts.IsEmpty)
+                    {
+                        string dades = detector.Decode(frame, punts);
+
+                        if (!string.IsNullOrEmpty(dades))
+                        {
+
+                            CvInvoke.PutText(frame, "QR: " + dades,
+                                new System.Drawing.Point(50, 50),
+                                FontFace.HersheySimplex, 0.8, new MCvScalar(0, 255, 0), 2);
+                        }
+                    }
+                }
+            }
+        }
+        private void btnEscanejar_click(object sender, EventArgs e)
+        {
+            _detectarAR = !_detectarAR;
+            btnEscanejar.Text = _detectarAR ? "Altura" : "Escanejar";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
